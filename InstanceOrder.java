66e863b6e396e7eb35127a731698fa7151ed57c9
郑超妮 <995621070@qq.com>
2019-10-27 21:10:44 +0800

    关于子类初始化触发父类初始化的验证代码
    关于子类初始化触发父类初始化的验证代码

--- InstanceOrder.java
+++ InstanceOrder.java
@@ -1,38 +1,44 @@
-class AA {
-    int a = initA();
-    private int initA() {
-        System.out.println("AA 初始化 a");
-        return 0;
-    }
-    {
-        System.out.println("AA 的构造代码块");
-    }
-    AA(int x) {
-        System.out.println("AA 的构造方法");
-    }
-}
 
-class BB extends AA {
-    int b = initB();
-    private int initB() {
-        System.out.println("BB 初始化 b");
-        return 0;
+    class AA {
+        int a = initA();
+        private int initA() {
+            System.out.println("AA 初始化 a");
+            return 0;
+        }
+        {
+            System.out.println("AA 的构造代码块");
+        }
+        AA(int x) {
+            System.out.println("AA 的构造方法");
+        }
     }
-    {
-        System.out.println("BB 的构造代码块");
-    }
-    BB(int x) {
-        super(x);
-        System.out.println("BB 的有参构造方法");
-    }
-    BB() {
-        this(100);
-        System.out.println("BB 的无参构造方法");
-    }
-}
 
-public class InstanceOrder {
-    public static void main(String[] args) {
-        new BB();
+    class BB extends AA {
+        int b = initB();
+        private int initB() {
+            System.out.println("BB 初始化 b");
+            return 0;
+        }
+        {
+            System.out.println("BB 的构造代码块");
+        }
+        BB(int x) {
+            super(x);//子类的有参构造调用父类的构造方法。super出现在第一行
+            System.out.println("BB 的有参构造方法");
+        }
+        BB() {
+            this(100);//再执行自己的无参构造，this调用本类
+            System.out.println("BB 的无参构造方法");
+        }
     }
-}
+
+
+    public class InstanceOrder {
+
+        public static void main(String[] args) {
+            new BB();
+        }
+
+    }
+
+
