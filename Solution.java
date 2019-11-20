2ff59f18fa654c41e7fa4d3c6fef263806392b41
郑超妮 <995621070@qq.com>
2019-11-20 11:45:15 +0800

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,41 +1,24 @@
 public class Solution {
-    CNode copy(CNode head) {//copy是Solution类下的方法
-        if (head == null) {
-            return null;
-        }
 
-        CNode p1 = head;
-        while (p1 != null) {//新旧挂钩
-            CNode p2 = new CNode(p1.val);//拷贝结点
 
-            p2.next = p1.next;
-            p1.next = p2;
-
-            p1 = p2.next;
-        }
-
-        p1 = head;
-        while (p1 != null) {//复制指向   让新的1指向新的2
-            CNode p2 = p1.next;
-            if (p1.random != null) {
-                p2.random = p1.random.next;
+        public int[] multiply(int[] A) {
+            int length = A.length;
+            int[] B = new int[length];
+            if(length != 0 ){
+                B[0] = 1;
+                //计算下三角连乘
+                for(int i = 1; i < length; i++){
+                    B[i] = B[i-1] * A[i-1];
+                }
+                int temp = 1;
+                //计算上三角
+                for(int j = length-2; j >= 0; j--){
+                    temp *= A[j+1];
+                    B[j] *= temp;
+                }
             }
-
-            p1 = p2.next;
+            return B;
         }
 
-        p1 = head;
-        CNode newHead = head.next;//创造新链表的头结点
-        while (p1 != null) {//拆开新旧
-            CNode p2 = p1.next;
-            p1.next = p2.next;
-            if (p2.next != null) {
-                p2.next = p2.next.next;
-            }
 
-            p1 = p1.next;//相当于更新cur
-        }
-
-        return newHead;
-    }
 }
