75eecd48448391679d5bc41a5735894fbbd39ada
郑超妮 <995621070@qq.com>
2019-12-17 23:11:16 +0800

    输入二叉树的前序遍历和中序遍历，请重建出该二叉树
    输入二叉树的前序遍历和中序遍历，请重建出该二叉树

--- Solution.java
+++ Solution.java
@@ -1,26 +1,41 @@
-import java.util.LinkedList;
-
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+import java.util.HashMap;
 public class Solution {
-    public ListNode Merge(ListNode list1,ListNode list2) {
-        if(list1==null){
-            return  list2;
+
+      class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
+
+
+    public  TreeNode reConstructBinaryTree(int[] pre, int[] in) {
+        int len = pre.length;
+        TreeNode head = new TreeNode(pre[0]);
+        return reConstructBinaryTree(pre, 0, len - 1, in, 0, len - 1);
+    }
+
+    public  TreeNode reConstructBinaryTree(int[] pre, int pstart, int pend, int[] in, int istart, int iend) {
+        if (pstart > pend || istart > iend) {
+            return null;
         }
-        if(list2==null){
-            return  list1;
+        TreeNode treeNode = new TreeNode(pre[pstart]);
+        for (int i = istart; i <= iend; ++i) {
+            if (in[i] == pre[pstart]) {
+                // i-istart=左节点个数
+                treeNode.left = reConstructBinaryTree(pre, pstart + 1, pstart + i - istart, in, istart, i - 1);
+                treeNode.right = reConstructBinaryTree(pre, pstart + i - istart+1, pend, in, i + 1, iend);
+            }
         }
-
-
-        if(list1.val<=list2.val){
-            list1.next=Merge(list.next,list2);
-
-            return list1;
-        }else{
-            list2.next=Merge(list1,list2.next);
-            return list2;
-        }
-
-
-
-
+        return treeNode;
     }
 }
