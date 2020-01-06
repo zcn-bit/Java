9ee66a9ce3d976b4d4565a8dca30b97ec5f1c3ce
郑超妮 <995621070@qq.com>
2020-01-06 21:49:25 +0800

    一个数组中除了两个数字之外，其他数字都出现了两次，找出这两个只出现一次的数字
    一个数组中除了两个数字之外，其他数字都出现了两次，找出这两个只出现一次的数字

---
+++ FindNumberAppearOnce.java
@@ -0,0 +1,35 @@
+public class FindNumberAppearOnce {
+    public void FindNumsAppearOnce(int[] array, int[] num1, int[] num2)    {
+        int length = array.length;
+        if(length == 2){
+            num1[0] = array[0];
+            num2[0] = array[1];
+            return;
+        }
+        int bitResult = 0;
+        for(int i = 0; i < length; ++i){
+            bitResult ^= array[i];
+        }
+        int index = findFirst1(bitResult);
+        for(int i = 0; i < length; ++i){
+            if(isBit1(array[i], index)){
+                num1[0] ^= array[i];
+            }else{
+                num2[0] ^= array[i];
+            }
+        }
+    }
+
+    private int findFirst1(int bitResult){
+        int index = 0;
+        while(((bitResult & 1) == 0) && index < 32){
+            bitResult >>= 1;
+            index++;
+        }
+        return index;
+    }
+
+    private boolean isBit1(int target, int index){
+        return ((target >> index) & 1) == 1;
+    }
+}
