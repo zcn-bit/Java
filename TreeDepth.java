10c12254b20200e743a44d1bf51fcd9114e3cf0c
郑超妮 <995621070@qq.com>
2020-01-06 21:55:22 +0800

    输入一颗二叉树，求该树的深度
    输入一颗二叉树，求该树的深度

---
+++ TreeDepth.java
@@ -0,0 +1,40 @@
+
+
+    import java.util.Queue;
+import java.util.LinkedList;
+    public class TreeDepth {
+   class TreeNode{
+       TreeNode left=null;
+       TreeNode right=null;
+       int val;
+       TreeNode(int val){
+           this.val=val;
+       }
+        }
+        public int TreeDepth(TreeNode pRoot)
+        {
+            if(pRoot == null){
+                return 0;
+            }
+            Queue<TreeNode> queue = new LinkedList<TreeNode>();
+            queue.add(pRoot);
+            int depth = 0, count = 0, nextCount = 1;
+            while(queue.size()!=0){
+                TreeNode top = queue.poll();
+                count++;
+                if(top.left != null){
+                    queue.add(top.left);
+                }
+                if(top.right != null){
+                    queue.add(top.right);
+                }
+                if(count == nextCount){
+                    nextCount = queue.size();
+                    count = 0;
+                    depth++;
+                }
+            }
+            return depth;
+        }
+    }
+
