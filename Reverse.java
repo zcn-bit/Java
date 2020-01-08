2ea713dc98cc755173ba817d2162c067208ccc97
郑超妮 <995621070@qq.com>
2020-01-08 22:57:59 +0800

    输入一句英文，将每个单词看作一个整体，然后输出逆序每个单词后的英文句子
    输入一句英文，将每个单词看作一个整体，然后输出逆序每个单词后的英文句子

---
+++ Reverse.java
@@ -0,0 +1,18 @@
+public class Reverse {
+
+    public String ReverseSentence(String str) {
+        if(str.trim().equals("")){
+            return str;
+        }
+        String[] a = str.split(" ");
+        StringBuffer o = new StringBuffer();
+        int i;
+        for (i = a.length; i >0;i--){
+            o.append(a[i-1]);
+            if(i > 1){
+                o.append(" ");
+            }
+        }
+        return o.toString();
+    }
+}
