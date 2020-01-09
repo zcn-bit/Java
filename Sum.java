e94b2f780ba4ec256784e36f45efad9ea1a62af9
郑超妮 <995621070@qq.com>
2020-01-09 16:13:44 +0800

    求两个整数之和，在函数体内不得使用+，-，*，/,四则运算符号
    求两个整数之和，在函数体内不得使用+，-，*，/,四则运算符号

---
+++ Sum.java
@@ -0,0 +1,12 @@
+public class Sum {
+
+    public int Add(int num1,int num2) {
+        while (num2!=0) {
+            int temp = num1^num2;
+            num2 = (num1&num2)<<1;
+            num1 = temp;
+        }
+        return num1;
+    }
+}
+
