d2776d1ff33c595cd3e3ffd4f67ce99f2188420f
郑超妮 <995621070@qq.com>
2020-01-19 23:55:19 +0800

    请找出数组中任意一个重复的数字
    请找出数组中任意一个重复的数字

---
+++ Duplication.java
@@ -0,0 +1,15 @@
+public class Duplication {
+
+    public boolean duplicate(int numbers[], int length, int[] duplication) {
+        boolean[] k = new boolean[length];
+        for (int i = 0; i < k.length; i++) {
+            if (k[numbers[i]] == true) {
+                duplication[0] = numbers[i];
+                return true;
+            }
+            k[numbers[i]] = true;
+        }
+        return false;
+    }
+
+}
