6047152a427a906d8cd83650bdf5ad0b968463a5
郑超妮 <995621070@qq.com>
2020-01-20 22:40:08 +0800

    请实现一个函数用来匹配包括‘.’和‘*’的正则表达式
    请实现一个函数用来匹配包括‘.’和‘*’的正则表达式

--- Solution.java
+++ Solution.java
@@ -1,8 +1,37 @@
 public class Solution {
 
-    public int Sum_Solution(int n) {
-        int sum = n;
-        boolean t = (sum > 0) && ((sum += Sum_Solution(n-1)) > 0);
-        return sum;
+    public boolean match(char[] str, char[] pattern) {
+        if(str == null || pattern == null) {
+            return false;
+        }
+        return matchCore(str, 0, pattern, 0);
+    }
+
+    private boolean matchCore(char[] cs, int csIndex, char[] pattern, int patternIndex) {
+        // 匹配结束，cs和pattern同时结束
+        if(csIndex == cs.length && patternIndex == pattern.length) {
+            return true;
+        }
+        // 匹配结束，cs比pattern先结束
+        if(csIndex != cs.length && patternIndex == pattern.length) {
+            return false;
+        }
+        // 如果第二个字符是*，判断第一个字符是否相同，若相同，分3种情况
+        // 如果不同，模式跳过两个字符
+        if(patternIndex + 1 < pattern.length && pattern[patternIndex + 1] == '*') {
+            if ((csIndex != cs.length && pattern[patternIndex] == cs[csIndex])
+                    || pattern[patternIndex] == '.' && csIndex != cs.length) {
+                return matchCore(cs, csIndex + 1, pattern, patternIndex + 2) // 跳转到下一个状态
+                        || matchCore(cs, csIndex + 1, pattern, patternIndex) // 保持当前状态，继续匹配
+                        || matchCore(cs, csIndex, pattern, patternIndex + 2);// 忽略*
+            } else {
+                return matchCore(cs, csIndex, pattern, patternIndex + 2); // 忽略*
+            }
+        }
+
+        if(csIndex != cs.length && (cs[csIndex] == pattern[patternIndex] || pattern[patternIndex] == '.')) {
+            return matchCore(cs, csIndex+1, pattern, patternIndex+1);
+        }
+        return false;
     }
 }
