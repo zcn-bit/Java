3b288502b4dd639b068cff3d4b143a04cde2a9e3
郑超妮 <995621070@qq.com>
2020-01-29 19:27:12 +0800

    判断数列的第l个到第r个有多少个数可以被3整除？
    有一个数列;1,12,123,1234,....123456789......判断数列的第l个到第r个有多少个数可以被3整除？

--- Solution.java
+++ Solution.java
@@ -1,37 +1,17 @@
 public class Solution {
 
-    public boolean match(char[] str, char[] pattern) {
-        if(str == null || pattern == null) {
-            return false;
-        }
-        return matchCore(str, 0, pattern, 0);
-    }
 
-    private boolean matchCore(char[] cs, int csIndex, char[] pattern, int patternIndex) {
-        // 匹配结束，cs和pattern同时结束
-        if(csIndex == cs.length && patternIndex == pattern.length) {
-            return true;
-        }
-        // 匹配结束，cs比pattern先结束
-        if(csIndex != cs.length && patternIndex == pattern.length) {
-            return false;
-        }
-        // 如果第二个字符是*，判断第一个字符是否相同，若相同，分3种情况
-        // 如果不同，模式跳过两个字符
-        if(patternIndex + 1 < pattern.length && pattern[patternIndex + 1] == '*') {
-            if ((csIndex != cs.length && pattern[patternIndex] == cs[csIndex])
-                    || pattern[patternIndex] == '.' && csIndex != cs.length) {
-                return matchCore(cs, csIndex + 1, pattern, patternIndex + 2) // 跳转到下一个状态
-                        || matchCore(cs, csIndex + 1, pattern, patternIndex) // 保持当前状态，继续匹配
-                        || matchCore(cs, csIndex, pattern, patternIndex + 2);// 忽略*
-            } else {
-                return matchCore(cs, csIndex, pattern, patternIndex + 2); // 忽略*
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int count = 0;
+        for(int i=n;i<=m;i++){
+            if(i%3%2==0){
+                count++;
             }
         }
+        System.out.println(count);
 
-        if(csIndex != cs.length && (cs[csIndex] == pattern[patternIndex] || pattern[patternIndex] == '.')) {
-            return matchCore(cs, csIndex+1, pattern, patternIndex+1);
-        }
-        return false;
     }
 }
