dfb9556f2df3cff482397fef2af1e60ad2477cfd
郑超妮 <995621070@qq.com>
2020-01-29 19:50:52 +0800

    计算一下至少需要多少盏路灯？
    一条长度为n的道路，需要安装路灯的用.表示，不需要照亮的用x表示，计算一下至少需要多少盏路灯？

--- Main.java
+++ Main.java
@@ -1,20 +1,23 @@
 
-
- import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
-    public class Main{
-        public static void main(String[] args){
-            Scanner in = new Scanner(System.in);
-            while(in.hasNext()){
-                char[] c = in.next().toCharArray();
-                StringBuffer sb = new StringBuffer();
-                Set<Character> set = new HashSet<Character>();
-                for(int i = 0;i<c.length;i++){
-                    if(set.add(c[i]))//很巧妙
-                        sb.append(c[i]);
+import java.util.*;
+//贪心算法求解：1.当遇到第一个'.'时，表示该位置需要被照亮，此时不安装路灯，在它的下一个位置安装路灯，即sum+1;
+//因为该路灯位置的下一个位置已经被照亮了，因此下标+2
+//遇到‘X’时跳过，因为不需要安装
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i = 0; i < t; i++){
+            int n = sc.nextInt();
+            String s = sc.next();
+            int sum = 0;
+            for(int j = 0; j < n; j++){
+                if(s.charAt(j) == '.'){
+                    sum++;
+                    j = j + 2;
                 }
-                System.out.println(sb.toString());
             }
+            System.out.println(sum);
         }
     }
+}
