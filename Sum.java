ed5e107275b399a0ebe9a6ad4cb78f57f7d74437
郑超妮 <995621070@qq.com>
2020-02-10 20:24:25 +0800

    已知深渊有N阶台阶，并且每次月神仅可以往上爬2的整数次幂个台阶，月神有多少种方法爬出深渊？
    已知深渊有N阶台阶，并且每次月神仅可以往上爬2的整数次幂个台阶，月神有多少种方法爬出深渊？

--- Sum.java
+++ Sum.java
@@ -1,12 +1,29 @@
+
+/*
+动态规划。类似于跳台阶。
+用递归会超时。
+取模防止溢出。
+*/
+import java.util.*;
 public class Sum {
 
-    public int Add(int num1,int num2) {
-        while (num2!=0) {
-            int temp = num1^num2;
-            num2 = (num1&num2)<<1;
-            num1 = temp;
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int T = in.nextInt();
+        int[] dp = new int[1001];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= 1000; i++) {
+            int tmp = 1;
+            while (tmp <= i) {
+                dp[i] += dp[i - tmp];
+                tmp *= 2;
+                dp[i] %= 1000000000 + 3;
+            }
         }
-        return num1;
+        while (T-- > 0) {
+            int n = in.nextInt();
+            System.out.println(dp[n]);
+        }
     }
 }
-
