347966766f41baa1e44403d29bdfd608bbb175fb
郑超妮 <995621070@qq.com>
2020-03-02 18:54:16 +0800

    学分绩点
    北京大学对本科生的成绩施行平均学分绩点制(GPA)。既将学生的实际考分根据不同的学科的不同学分按定的公式进行计算。公式如下: 实际成绩绩点90-  1004.085893.782- -84 3378 -81 3075-一77 27 72- 742368-售71 2.064- 67 1560-一63 1060以下01.一门课程的学分绩点=该课绩点“该课学分2.总评绩点=所有学科绩点之和/所有课程学分之和现要求你编写程序求出某人A的总评绩点(GPA)

--- Score.java
+++ Score.java
@@ -1,30 +1,70 @@
+import java.util.Scanner;
 
-import java.util.*;
-//思路就是存储每个方块出现的次数，最后求次数最小的方块，即为得分数。
 public class Score {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        while(scanner.hasNext()){
 
-    public static void main(String[] args){
-        Map<Integer, Integer> map = new HashMap<>();
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        for(int i = 0; i < m; i++){
-            int temp = sc.nextInt();
-            if(map.containsKey(temp)){
-                map.put(temp, map.get(temp) + 1);
-            }else{
-                map.put(temp, 1);
+
+            int n=scanner.nextInt();
+            int []score=new int[n];//定义数组预先得到大小   new开辟数组空间
+            int[] grade=new int[n];
+            double []coursePoint=new double[n];
+            for(int i=0;i<n;i++){//相应学分
+                score[i]=scanner.nextInt();
             }
-        }
-        //判断每个元素出现的次数，次数最小的就是得分
-        int score = Integer.MAX_VALUE;
-        if(map.size() == n){ //保证n列上只出现n个不同的方块
-            for(Integer key : map.keySet()){
-                score = Math.min(score, map.get(key));
+            for(int i=0;i<n;i++){//实际得分
+                grade[i]=scanner.nextInt();
             }
-        }else{
-            score = 0;
+             for(int i=0;i<n;i++){   //遍历实际得分
+                if(grade[i]>=90&&grade[i]<=100){
+                    coursePoint[i]=4.0;//相应绩点
+                }
+                 if(grade[i]>=85&&grade[i]<=89){
+                     coursePoint[i]=3.7;
+                 }
+                 if(grade[i]>=82&&grade[i]<=84){
+                     coursePoint[i]=3.3;
+                 }
+                 if(grade[i]>=78&&grade[i]<=81){
+                      coursePoint[i]=3.0;
+                 }
+
+                 if(grade[i]>=75&&grade[i]<=77){
+                     coursePoint[i]=2.7;
+                 }
+                 if(grade[i]>=72&&grade[i]<=74){
+                     coursePoint[i]=2.3;
+                 }
+                 if(grade[i]>=68&&grade[i]<=71){
+                     coursePoint[i]=2.0;
+                 }
+                 if(grade[i]>=64&&grade[i]<=67){
+                     coursePoint[i]=1.5;
+                 }
+                 if(grade[i]>=60&&grade[i]<=63){
+                     coursePoint[i]=1.0;
+                 }
+                 if(grade[i]<60){
+                     coursePoint[i]=0;
+                 }
+             }
+             double[] coursePoints=new double[n];
+             for(int i=0;i<n;i++){
+              coursePoints[i]=coursePoint[i]*score[i];
+             }//每门学科的  学分绩点    不要忽略每一个要求
+            double courseSum=0;
+            for(int i=0;i<n;i++){
+                courseSum=courseSum+coursePoints[i];
+            }
+            int scores=0;
+            for(int i=0;i<n;i++) {
+                scores=scores+score[i] ;
+            }//所有学分之和
+
+
+            double GPA=courseSum/scores;
+            System.out.printf("%.2f",GPA);
         }
-        System.out.print(score);
     }
 }
