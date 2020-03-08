8e499fb77a2c4e4afdd75bac886a13325021b53a
郑超妮 <995621070@qq.com>
2020-03-08 18:06:16 +0800

    牛客网：密码验证
    密码验证（至少包含所要求的三种）

--- Number.java
+++ Number.java
@@ -1,30 +1,23 @@
-
 import java.util.Scanner;
 
 public class Number {
-
-    public static long result = 1;
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        long total = scanner.nextLong();
-        long[] base = new long[n];
-        for (int i = 0; i < base.length; i++) {
-            base[i] = scanner.nextLong();
-        }
-        dfs(base, 0, total, 0);
-        System.out.println(result);
+        Scanner scanner=new Scanner(System.in);
+        while(scanner.hasNext()){
+            int n=scanner.nextInt();
+            if(n<2||n>=100){
+                return;
+            }//
 
-    }
-
-    public static void dfs(long[] base, int index, long total, long current) {
-        if(index == base.length) {
-            return;
+            int m=n*n;
+            String string=String.valueOf(m);//把这个十进制的long看作一个串
+            String str=string.substring(1,string.length());//不要串的第一个xaibiao从0开始
+            String strn=String.valueOf(n);//相当于两个串再做比较
+            if(str.equals(strn)){//6.（4）
+                System.out.println("YES!");
+            }else {
+                System.out.println("NO!");
+            }
         }
-        if(current + base[index] <= total) {
-            result++;
-            dfs(base, index + 1, total, current  + base[index]);
-        }
-        dfs(base, index + 1, total, current);
     }
 }
