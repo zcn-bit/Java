0ebc6e23b5a9c4ae4fbe3979dccda5ebe37965c6
郑超妮 <995621070@qq.com>
2020-03-11 18:29:19 +0800

    牛客网题目：说反话
    牛客网题目：说反话（逆序输出单词顺序）

--- Reverse.java
+++ Reverse.java
@@ -1,18 +1,21 @@
-public class Reverse {
+import java.util.Scanner;
 
-    public String ReverseSentence(String str) {
-        if(str.trim().equals("")){
-            return str;
+public class Reverse {
+    public static void reverseString(String string){
+        String[]  str=string.split(" ");
+       // String[] str2=new String[str.length];
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<str.length;i++){
+            sb.insert(0," "+str[i]);//每次前插
         }
-        String[] a = str.split(" ");
-        StringBuffer o = new StringBuffer();
-        int i;
-        for (i = a.length; i >0;i--){
-            o.append(a[i-1]);
-            if(i > 1){
-                o.append(" ");
-            }
+
+        System.out.println(sb.toString().trim());//把第一个空格修剪了
+    }
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        while (scanner.hasNext()){
+            String string=scanner.nextLine();
+            reverseString(string);
         }
-        return o.toString();
     }
 }
