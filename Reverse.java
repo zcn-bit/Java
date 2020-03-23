a4907abc8cdbea0e95633010998dfebebd8dd89a
郑超妮 <995621070@qq.com>
2020-03-23 21:33:22 +0800

    牛客网：句子逆序
    牛客网：句子逆序

--- Reverse.java
+++ Reverse.java
@@ -1,21 +1,23 @@
 import java.util.Scanner;
 
 public class Reverse {
-    public static void reverseString(String string){
-        String[]  str=string.split(" ");
-       // String[] str2=new String[str.length];
-        StringBuilder sb=new StringBuilder();
-        for(int i=0;i<str.length;i++){
-            sb.insert(0," "+str[i]);//每次前插
+    public  static String reverse(String sentence){
+        String [] chars=sentence.split(" ");
+        StringBuffer sb=new StringBuffer();
+        for(int i=0;i<chars.length;i++){
+            sb.insert(0," "+chars[i]);
         }
+        String result=sb.toString().trim();//修剪前后空格
+        return result;
 
-        System.out.println(sb.toString().trim());//把第一个空格修剪了
     }
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
-        while (scanner.hasNext()){
-            String string=scanner.nextLine();
-            reverseString(string);
+        while(scanner.hasNext()){
+            String str=scanner.nextLine();
+            System.out.println( reverse(str));
+
+
         }
     }
 }
