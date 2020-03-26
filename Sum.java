c29f84522afd5a6de9ae32ef9addb0aa7a7a1592
郑超妮 <995621070@qq.com>
2020-03-26 08:49:12 +0800

    牛客网：求和
    牛客网：求和

--- Sum.java
+++ Sum.java
@@ -1,29 +1,41 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
-/*
-动态规划。类似于跳台阶。
-用递归会超时。
-取模防止溢出。
-*/
-import java.util.*;
 public class Sum {
+    static ArrayList<ArrayList<Integer>>  res=new  ArrayList<ArrayList<Integer>>();
+    static ArrayList<Integer> list=new ArrayList<>();
+    //类变量，全局变量
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int T = in.nextInt();
-        int[] dp = new int[1001];
-        dp[0] = 1;
-        dp[1] = 1;
-        for (int i = 2; i <= 1000; i++) {
-            int tmp = 1;
-            while (tmp <= i) {
-                dp[i] += dp[i - tmp];
-                tmp *= 2;
-                dp[i] %= 1000000000 + 3;
+        Scanner scanner=new Scanner(System.in);
+        while(scanner.hasNext()){
+            int n=scanner.nextInt();
+            int m=scanner.nextInt();
+            dfs(1,m,n);
+            for(ArrayList<Integer> l:res) {//遍历res
+                int i = 0;
+                for (; i <l.size()-1;i++) {//遍历list
+                    System.out.print(l.get(i)+" ");
+
+                }
+                System.out.println(l.get(i));//最后一个元素单独处理不加空格
             }
         }
-        while (T-- > 0) {
-            int n = in.nextInt();
-            System.out.println(dp[n]);
+    }
+
+
+
+    public static void dfs(int index,int count,int n){//n个数，和为count
+        if(count==0){
+            res.add(new ArrayList<>(list));
+        }else {
+            for (int j=index;j<=count&&j<=n;j++){//下限
+                list.add(j);//1
+                dfs(j+1,count-j,n);
+                list.remove(list.size()-1);
+            }
+
         }
+
     }
 }
