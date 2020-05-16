8bfe6c813f413b20f436371e0111a7358155e3f1
郑超妮 <995621070@qq.com>
2020-05-16 14:33:03 +0800

    两两相邻交换排序，最少需要交换几次？
    两两相邻交换排序，最少需要交换几次？

--- Main3.java
+++ Main3.java
@@ -1,38 +1,45 @@
-import sun.applet.Main;
 
-import java.util.Scanner;
 
+        import java.util.Scanner;
+        import static java.lang.System.in;
 public class Main3 {
-    private long a;
-    private long b;
-
-    public Main3(long a, long b){
-        this.a = a;
-        this.b = b;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(in);
+        int n = sc.nextInt();
+        int[] data = new int[n];
+        for (int i = 0; i < n; i++) {
+            data[i] = sc.nextInt();
+        }
+        temp = new int[n];
+        mergeWay(data, 0, n - 1);
+        System.out.println(count);
     }
-
-    @SuppressWarnings("resource")
-    public static void main(String[] args){
-        Scanner in = new Scanner(System.in);
-        long x = in.nextLong();
-        long y = in.nextLong();
-        Main3 m = new Main3(x, y);
-        System.out.println(m.test());
+    public static void mergeWay(int[] data, int begin, int end) {
+        if (begin >= end) {
+            return;
+        }
+        int mid = begin + ((end - begin) >> 1);
+        mergeWay(data,begin,mid);
+        mergeWay(data,mid+1,end);
+        merge(data, begin, end, mid);
     }
-
-    public int test(){
-        if (a <= b) {
-            int count = 0;
-            for(long i=a; i<=b; i++) {
-                if(i%3 != 1) {//14
-
-                    count ++;
-                }
-            }
-            return count;
-        } else {
-            return 0;
+    public static int[] temp;
+    public static int count = 0;
+    private static void merge(int[] data, int begin, int end, int mid) {
+        int i = begin, j = mid + 1;
+        int k = i;
+        while (i <= mid && j <= end) {
+            count += data[i] <= data[j] ? 0 : mid - i + 1;//这里加的逆序对是这么多，而不是加1
+            temp[k++] = data[i] <= data[j] ? data[i++] : data[j++]; //先写上面，这里i，j已变化
+        }
+        while (i <= mid) {
+            temp[k++] = data[i++];
+        }
+        while (j <= end) {
+            temp[k++] = data[j++];
+        }
+        for (int h = begin; h < k; h++) {
+            data[h] = temp[h];
         }
     }
 }
-
