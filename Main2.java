00df112f72793f5f27a154a816553ca8a798f100
郑超妮 <995621070@qq.com>
2020-05-27 22:49:37 +0800

    牛客网：航线
    牛客网：航线

--- Main2.java
+++ Main2.java
@@ -1,122 +1,72 @@
 
-
-        import java.util.*;
-public class Main2{
-
-    public static void main(String[] args){
-        //输入
-        Scanner scanner = new Scanner(System.in);
-        int num = scanner.nextInt();
-        if(num==0||num==1) {
-            System.out.println(num);
-            return;
-        }
-        scanner.nextLine();
-        String[] str = new String[num];
-        for(int i=0;i<num;i++) {
-            if(scanner.hasNext()) {
-                String s = scanner.nextLine();
-                str[i] = s;
+import java.util.Scanner;
+class Line{
+    boolean visited;
+    int begin;
+    int end;
+    int time;
+    public Line(){//构造方法用于初始化属性
+        visited=false;
+        time=Integer.MAX_VALUE;
+    }
+    public  void set(int t,int b,int e){
+        this.begin=b;
+        this.end=e;
+        this.time=t;
+    }
+}
+public class Main2 {
+    public static  int dijkstra(Line [][] lines,int n){
+        int [] time=new int[n+1];
+        for(int i=2;i<=n;i++){
+            if(lines[1][i].time<lines[1][i].begin){
+                time[i]=lines[1][i].time;
+            }else {
+                time[i]=lines[1][i].time+lines[1][i].end;
             }
-        }
-        scanner.close();
-        if(str==null){
-            System.out.println(0);
-            return;
-        }
-        ArrayList<Node> arr = new ArrayList<Node>();
-        for(String s:str) {
-            String[] s1 = s.split(" ");
-            int leftval = Integer.valueOf(s1[0]);
-            int rightval = Integer.valueOf(s1[1]);
-            arr.add(new Node(leftval,rightval));
-        }
-        //把第一张海报贴到墙上
-        ArrayList<ArrayList<Node>> Wall = new ArrayList<ArrayList<Node>>();
-        ArrayList<Node> poster1 = new ArrayList<Node>();
-        poster1.add(arr.get(0));
-        Wall.add(poster1);
-        //每加入一张海报，先对墙上的每一个海报段求补集，修改原来的海报段露出的部分的区间
-        for(int i=1;i<arr.size();i++) {//新加入的海报
-            for(int j=0;j<Wall.size();j++) {//已经贴好的每一张海报
-                int index = Wall.get(j).size();
-                for(int k=0;k<index;k++) {//每张海报段
-                    ArrayList<Node> newPosterDuan = Overlap
-                            (Wall.get(j).get(k),arr.get(i));//老海报段对新海报的补集
-                    if(newPosterDuan==null) {
-                        Wall.get(j).set(k,null);
-                    }else {
-                        Wall.get(j).remove(k);
-                        for(int m=0;m<newPosterDuan.size();m++) {
-                            Wall.get(j).add(k,newPosterDuan.get(m));
+            time[1]=1;
+            lines[1][1].visited=true;
+            int v=1;
+            while (v!=n){
+                int minTime=Integer.MIN_VALUE;
+                v=1;
+                for(int j=1;j<=n;j++){
+                    if(!lines[j][j].visited&&lines[v][j].time<Integer.MAX_VALUE){
+                        int tmp;
+                        if(lines[v][j].time+time[v]<lines[v][j].begin||time[v]>lines[v][j].end){
+                           tmp=lines[v][j].time+time[v];
+                        }else {
+                            tmp=lines[v][j].end+lines[v][j].time;
+                            time[j]=time[j]<tmp?time[j]:tmp;
                         }
                     }
                 }
-            }
-            ArrayList<Node> temp =new ArrayList<Node>();//加入新海报
-            temp.add(arr.get(i));
-            Wall.add(temp);
-        }
-        //求总的不为空的海报数量
-        int count = 0;
-        for(int i=0;i<Wall.size();i++) {
-            boolean flag = false;
-            ArrayList<Node> temp = Wall.get(i);
-            for(int k=0;k<temp.size();k++) {
-                if(temp.get(k)!=null) {
-                    flag = true;
-                }
-            }
-            if(flag==true) {
-                count++;
+
+
             }
         }
-        System.out.println(count);
+        return time[n]+1;
     }
-    public static ArrayList<Node> Overlap(Node node1,Node node2) {//求node1的补集
-        ArrayList<Node> newPosterDuan = new ArrayList<Node>();
-        if(node1==null) {
-            return null;
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();//N个港
+        int m=scanner.nextInt();//M个航线
+        Line[][] lines=new Line[n+1][n+1];
+        for(int i=1;i<=n;i++){
+            for (int j=1;j<=n;j++){
+                lines[i][j]=new Line();
+            }
         }
-        if(node1.left>=node2.left && node1.right<=node2.right ) {
-            return null;
+        for(int i=0;i<m;i++){
+            int p=scanner.nextInt();
+            int q=scanner.nextInt();
+            int t=scanner.nextInt();
+            int b=scanner.nextInt();
+            int e=scanner.nextInt();
+            lines[p][q].set(t,b,e);
+            lines[q][p]=lines[p][q];
         }
-        else if(node1.left<=node2.left && node1.right<=node2.right &&
-                node2.left<=node1.right) {
-            node1.right = node2.left;
-            if(node1.left!=node1.right) {
-                newPosterDuan.add(node1);
-            }
-        }else if(node1.left<=node2.right&&node1.left>=node2.left&&
-                node1.right>=node2.right) {
-            node1.left = node2.right;
-            if(node1.left!=node1.right) {
-                newPosterDuan.add(node1);
-            }
-        }else if(node1.left<=node2.left&&node1.right>=node2.right) {
-            Node newNode1 = new Node(node1.left,node2.left);
-            Node newNode2 = new Node(node2.right,node1.right);
-            if(node1.left!=node1.right) {
-                newPosterDuan.add(newNode1);
-            }
-            if(node2.left!=node2.right) {
-                newPosterDuan.add(newNode2);
-            }
-        }else {
-            newPosterDuan.add(node1);
-        }
-        return newPosterDuan;
-    }
-}
-class Node{
-    int left;
-    int right;
-    public Node(int left,int right) {
-        this.left = left;
-        this.right = right;
-    }
-    @Override
-    public String toString() {
-        return "Node [left=" + left + ", right=" + right + "]";
+        System.out.println(dijkstra(lines,n));
+
     }
 }
