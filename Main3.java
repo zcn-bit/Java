e9655fe8c4f715c1def1f1e6bbd4a55d85f52c32
郑超妮 <995621070@qq.com>
2020-05-28 09:27:23 +0800

    牛客网：连续子数组最大和
    牛客网：连续子数组最大和

--- Main3.java
+++ Main3.java
@@ -1,45 +1,28 @@
 
 
-        import java.util.Scanner;
-        import static java.lang.System.in;
+import java.util.*;
 public class Main3 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(in);
-        int n = sc.nextInt();
-        int[] data = new int[n];
-        for (int i = 0; i < n; i++) {
-            data[i] = sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] nums = new int[N];
+        for (int i = 0; i < N; i++) {
+            nums[i] = sc.nextInt();
         }
-        temp = new int[n];
-        mergeWay(data, 0, n - 1);
-        System.out.println(count);
-    }
-    public static void mergeWay(int[] data, int begin, int end) {
-        if (begin >= end) {
-            return;
+
+        int max = nums[0],sum = 0;
+        for(int i = 0;i < nums.length;i++){
+            sum += nums[i];
+            //更新
+            if(sum > max)
+                max = sum;
+            //sum并不是记录最大连续和，只记录大于零的和，只要连续和小于0
+            //则重新开始计算和，因为nums[i]加上一个负数肯定比它本身小
+            if(sum < 0){
+                sum = 0;
+            }
         }
-        int mid = begin + ((end - begin) >> 1);
-        mergeWay(data,begin,mid);
-        mergeWay(data,mid+1,end);
-        merge(data, begin, end, mid);
-    }
-    public static int[] temp;
-    public static int count = 0;
-    private static void merge(int[] data, int begin, int end, int mid) {
-        int i = begin, j = mid + 1;
-        int k = i;
-        while (i <= mid && j <= end) {
-            count += data[i] <= data[j] ? 0 : mid - i + 1;//这里加的逆序对是这么多，而不是加1
-            temp[k++] = data[i] <= data[j] ? data[i++] : data[j++]; //先写上面，这里i，j已变化
-        }
-        while (i <= mid) {
-            temp[k++] = data[i++];
-        }
-        while (j <= end) {
-            temp[k++] = data[j++];
-        }
-        for (int h = begin; h < k; h++) {
-            data[h] = temp[h];
-        }
+        System.out.println(max);
     }
 }
