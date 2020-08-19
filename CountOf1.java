ba87fc1d7cc40c4a73fd8434a5e8a0f57904d33a
郑超妮 <995621070@qq.com>
2020-08-19 21:36:22 +0800

    整数中1的次数
    整数中1的次数

--- CountOf1.java
+++ CountOf1.java
@@ -1,16 +1,24 @@
-import java.util.Scanner;
 
+import java.util.*;
 public class CountOf1 {
-    public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        int n=scanner.nextInt();
-        int count=0;
-        while (n!=0){
-            if(n%2==1){
+    public int NumberOf1Between1AndN_Solution(int n) {
+        int count = 0;
+        // 循环 1->n
+        for(int i=1;i<=n;i++){
+            count += f(i);
+        }
+        return count;
+    }
+    // 判断有多少个1
+    public int f(int n){
+        String str = n+"";
+        char[] c = str.toCharArray();
+        int count = 0;
+        for(int i=0;i<c.length;i++){
+            if(c[i]=='1'){
                 count++;
             }
-            n=n/2;
         }
-        System.out.println(count);
+        return count;
     }
 }
