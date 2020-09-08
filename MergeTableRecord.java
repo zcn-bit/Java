e40bb6ddaea9480b515370e1f9d83e84d0c7c399
郑超妮 <995621070@qq.com>
2020-09-08 22:37:45 +0800

    合并表记录
    合并表记录

---
+++ MergeTableRecord.java
@@ -0,0 +1,25 @@
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class MergeTableRecord {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            int next = sc.nextInt();
+            TreeMap<Integer, Integer> map = new TreeMap<>();
+            for (int i = 0; i < next; i++) {
+                int key = sc.nextInt();
+                int value = sc.nextInt();
+                if (map.containsKey(key)) {
+                    map.put(key, map.get(key) + value);
+                } else {
+                    map.put(key, value);
+                }
+            }
+            for (Map.Entry<Integer, Integer> integerIntegerEntry : map.entrySet()) {
+                System.out.println(integerIntegerEntry.getKey() + " " + integerIntegerEntry.getValue());
+            }
+        }
+    }
+}
